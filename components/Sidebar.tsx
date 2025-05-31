a778f1d40f6dff6c6812a3c12a0d2ee48473a8d5
Zeeltech <zilodev831@gmail.com>
2025-05-31 22:05:30 +0100

    final touches

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -19,10 +19,10 @@ const Sidebar = ({ user }: SiderbarProps) => {
             src="/icons/logo.svg"
             width={34}
             height={34}
-            alt="Horizon logo"
+            alt="Royeh_Banking logo"
             className="size-[24px] max-xl:size-14"
           />
-          <h1 className="sidebar-logo">Horizon</h1>
+          <h1 className="sidebar-logo">Royeh_Banking</h1>
         </Link>
 
         {sidebarLinks.map((item) => {
